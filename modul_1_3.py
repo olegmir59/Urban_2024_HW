da10a68fdbe40e6772cd57b7579644e87ca9f148
olegmir59 <udom1@ya.ru>
2024-09-10 17:39:55 +0700

    Add File

---
+++ modul_1_3.py
@@ -0,0 +1,10 @@
+#  Практическая работа по уроку "Динамическая типизация"
+
+name = 'Олег'
+print('Name:', name)
+age = 64
+print('Age:', age)
+age = age +(-20)    # у меня жена младше на 20 лет
+print('New Age:', age)
+is_student = True
+print('Is_Student:', is_student)
