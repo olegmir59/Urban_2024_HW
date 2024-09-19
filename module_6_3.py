6f7075fa7c30d3c9764944ac5045ddd2dc26f6d2
olegmir59 <udom1@ya.ru>
2024-09-19 11:47:59 +0700

    Refactor

--- module_6_3.py
+++ module_6_3.py
@@ -2,48 +2,45 @@
 #  Цель: закрепить знания множественного наследования в Python.
 #
 # Задача "Мифическое наследование":
-
-class Horse():
-    def __init__(self, x_distance, sound='Frrr'):
-        self.x_distance = x_distance
-        self.sound = sound
+class Horse:
+    x_distance = 0
+    sound_hors = 'Frrr'
 
     def run(self, dx):
-        Horse.x_distance = dx
+        self.x_distance += dx
 
-
-class Eagle():
-    def __init__(self, y_distance, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
-        self.y_distance = self.y_distance + dy
+        self.y_distance += dy
 
 class Pegasus(Horse, Eagle):
-    def __init__(self, x_distance=0, y_distance=0, sound="UUUUU"):
-        Horse().__init__(x_distance)
-        Eagle().__init__(y_distance,sound)
-    #    Eagle().__init__(sound)
 
+    def __init__(self):
+         self.sound = super().sound_hors
+         self.sound = super().sound
 
     def move(self, dx, dy):
-        super().run(dx)
-        Eagle().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
-        return (Horse().x_distance, Eagle().y_distance)
+        return (self.x_distance, self.y_distance)
 
     def voice(self):
-        print(Eagle().sound)
+#        print(self.sound_hors)             # помним голос первого родителя тоже!
+        print(self.sound)
 
 
 p1 = Pegasus()
+
 print(Pegasus.mro())
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
-#p1.move(-5, 20)
-#print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
 
-#p1.voice()
+p1.voice()
