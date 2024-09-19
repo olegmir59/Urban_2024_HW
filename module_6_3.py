d529596425fcfc9f860a00515dad83e8c2252440
olegmir59 <udom1@ya.ru>
2024-09-19 11:59:21 +0700

    Refactor

--- module_6_3.py
+++ module_6_3.py
@@ -2,25 +2,30 @@
 #  Цель: закрепить знания множественного наследования в Python.
 #
 # Задача "Мифическое наследование":
+
+
 class Horse:
-    x_distance = 0
-    sound_hors = 'Frrr'
+    def __init__(self, *, x_distance=0, sound='Frrr'):
+        self.x_distance = x_distance
+        self.sound = sound
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
 
+
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance
+        self.sound = sound
 
     def fly(self, dy):
         self.y_distance += dy
 
-class Pegasus(Horse, Eagle):
 
+class Pegasus(Horse, Eagle):
     def __init__(self):
-         self.sound = super().sound_hors
-         self.sound = super().sound
+       super().__init__()
 
     def move(self, dx, dy):
         self.run(dx)
@@ -30,13 +35,11 @@ class Pegasus(Horse, Eagle):
         return (self.x_distance, self.y_distance)
 
     def voice(self):
-#        print(self.sound_hors)             # помним голос первого родителя тоже!
         print(self.sound)
 
 
 p1 = Pegasus()
 
-print(Pegasus.mro())
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
