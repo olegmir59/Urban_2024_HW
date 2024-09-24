327a7b435a577d66da03e1f761328ab614ac772a
olegmir59 <udom1@ya.ru>
2024-09-25 03:00:34 +0700

    Modify file

--- m_6_3.py
+++ m_6_3.py
@@ -1,2 +1,13 @@
-class Horse:
+class A:
+    def do_thing(self):
+        print('From A')
+class B(A):
+    def do_thing(self):
+        print('From B')
+class C(A):
+    def do_thing(self):
+        print('From C')
+class D(B, C):
     pass
+d = D()
+d.do_thing()
